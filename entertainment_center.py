0fc2a8b313333a6fd628928fd76bf4990b358e9c
leenpaws <s.chhaya@gmail.com>
2016-04-16 17:29:49 -0400

    Udacity Project

---
+++ entertainment_center.py
@@ -0,0 +1,31 @@
+import Movieclass
+import fresh_tomatoes
+
+# def_init is a contructor(function) creates space in memory for new instance Brad
+# instance ex. toy story
+toy_story = Movieclass.Movie("Toy Story",
+                             "A story of a boy and his toys that come to life",
+                             "http://s3-us-west-2.amazonaws.com/images.hellogiggles.com/uploads/2014/11/06/Toy-story-coloring-500x375c.jpg",
+                             'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+
+avatar=Movieclass.Movie("Avatar",
+                        "Cultural appropriation in space",
+                        "http://images2.fanpop.com/image/photos/9400000/movie-poster-avatar-the-movie-9459182-376-500.jpg",
+                        "https://www.youtube.com/watch?v=_i2RCBa3l-g")
+star_wars=Movieclass.Movie("Star Wars",
+                                "Priest kidnaps child for cult and eventual marriage to politician twice his age",
+                                "http://cdn.collider.com/wp-content/uploads/star-wars-episode-1-poster-398x600.jpg",
+                                "https://www.youtube.com/watch?v=I6hOlI9cg4o")
+
+matrix=Movieclass.Movie("The Matrix",
+                        "A depressed office worker joins a cult and destabilizes the government",
+                        "http://images.moviepostershop.com/the-matrix-movie-poster-1999-1010189516.jpg",
+                        "https://www.youtube.com/watch?v=_Ls19O-9p3s")
+#print(avatar.storyline)
+#avatar.showtrailer()
+#MAKE SURE to use double underscores in declaring function
+
+movies=[toy_story, avatar, star_wars, matrix]
+
+fresh_tomatoes.open_movies_page(movies)
+
